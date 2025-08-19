9d916caf87d86a027b71d31b5800ad2e70db40aa
MuhammadDevs <deenmuhammad19n@gmail.com>
2025-08-19 20:01:48 +0600

    added some code

--- src/Components/StudentData/StudentData.jsx
+++ src/Components/StudentData/StudentData.jsx
@@ -1,5 +1,5 @@
 import React, { use } from 'react';
-import { Bar, BarChart, Radar, RadarChart, XAxis, YAxis } from 'recharts';
+import { Bar, BarChart, Legend, PolarAngleAxis, PolarGrid, Radar, RadarChart, XAxis, YAxis } from 'recharts';
 
 const StudentData = ({marksPromise}) => {
     const studentMarksRes = use(marksPromise)
@@ -18,15 +18,18 @@ student.mine = mine;
     // console.log(chartData);
     return (
         <div>
-            <BarChart width={600} height={400} data={chartData}>
+            <BarChart width={600} height={500} data={chartData}>
                 <XAxis dataKey='name'></XAxis>
                 <YAxis></YAxis>
                 <Bar dataKey='math' fill='green'></Bar>
             </BarChart>
 
-            <RadarChart outerRadius={100} width={500} height={400} data={chartData}>
+            <RadarChart outerRadius={150} width={500} height={400} data={chartData}>
+                <PolarGrid></PolarGrid>
+                <PolarAngleAxis dataKey='math'></PolarAngleAxis>
                 <Radar name='Mike' dataKey='math' stroke="gray" fill="skyblue" fillOpacity={0.6}></Radar>
                 <Radar name="Lily" dataKey="mine" stroke="red" fill="gray" fillOpacity={0.6} />
+                <Legend></Legend>
             </RadarChart>
         </div>
     );
